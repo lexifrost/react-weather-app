61288e3377eeecc7954f657fd8fe4e8e965a0a7f
lexifrost <alexa.frost@outlook.com>
2024-06-02 09:38:10 +0200

    Created the basic functional weather search form

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      This project was coded by{" "}
+      <a href="https://github.com/lexifrost" target="_blank">
+        Lexi Frost
+      </a>
+      , is open-sourced on GitHub and hosted on Netflify.
+    </footer>
+  );
+}
---
+++ src/Weather.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Weather.css";
+
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "5863935ee9cca4c02ed68203f807c65b";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form className="searchForm" onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="enter a city.."
+        autoFocus
+        onChange={updateCity}
+        className="searchInput"
+      />
+      <input type="submit" value="Search" className="submitBtn" />
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div className="appContainer">
+        {form}
+        <ul className="weatherList">
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return <div className="appContainer">{form}</div>;
+  }
+}
