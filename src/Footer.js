b1a7ace23aadf44d15b0ea2779030d21481a09a4
lexifrost <alexa.frost@outlook.com>
2024-06-02 09:44:05 +0200

    Fixed footer link

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer>
       This project was coded by{" "}
-      <a href="https://github.com/lexifrost" target="_blank">
+      <a href="https://github.com/lexifrost" target="_blank" rel="noreferrer">
         Lexi Frost
       </a>
       , is open-sourced on GitHub and hosted on Netflify.
